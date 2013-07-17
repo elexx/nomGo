c6433d766d0540a184bf77b5a8a8f11550aa3304
Alexander Falb <fal3xx@gmail.com>
2013-07-17 08:58:14 +0200

    preparing for full crud - backend

--- routes/index.js
+++ routes/index.js
@@ -29,14 +29,38 @@
 		app.get('/mongo/dbs', function(req, res) {
 			mongoController.getDatabaseList(function(d) { res.json(d);} );
 		});
+
+		/** DATABASES **/
 		app.get('/mongo/dbs/:database', function(req, res) {
 			mongoController.getCollectionList(req.params.database, function(d) { res.json(d);} );
 		});
+		app.put('/mongo/dbs/:database', function(req, res) {
+			console.log('new database [%s]', req.params.database);
+		});
+		app.delete('/mongo/dbs/:database', function(req, res) {
+			console.log('delete database [%s]', req.params.database);
+		});
+
+		/** COLLECTIONS **/
 		app.get('/mongo/dbs/:database/:collection', function(req, res) {
 			mongoController.getDocumentList(req.params.database, req.params.collection, function(d) { res.json(d);} );
 		});
+		app.put('/mongo/dbs/:database/:collection', function(req, res) {
+			console.log('new collection [%s - %s]', req.params.database, req.params.collection);
+		});
+		app.delete('/mongo/dbs/:database/:collection', function(req, res) {
+			console.log('delete collection [%s - %s]', req.params.database, req.params.collection);
+		});
+
+		/** DOCUMENTS **/
 		app.get('/mongo/dbs/:database/:collection/:document', function(req, res) {
 			mongoController.getContent(req.params.database, req.params.collection, req.params.document, function(d) { res.json(d);} );
+		});
+		app.post('/mongo/dbs/:database/:collection', function(req, res) {
+			console.log('new document [%s - %s]', req.params.database, req.params.collection);
+		});
+		app.delete('/mongo/dbs/:database/:collection/:document', function(req, res) {
+			console.log('delete document [%s - %s - %s]', req.params.database, req.params.collection, req.params.document);
 		});
 	}
 
